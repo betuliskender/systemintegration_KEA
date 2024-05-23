5f28838fbf2a9d5766c7154bb93a3aebed3f8321
Betül Iskender <betul.iskender@outlook.dk>
2024-05-23 10:52:37 +0200

    06a done

--- 02._Data_Files/CSparser/obj/Debug/net7.0/CSparser.AssemblyInfo.cs
+++ 02._Data_Files/CSparser/obj/Debug/net7.0/CSparser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSparser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e51664add7b8d0392eec357504ab8b764001550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a621796cddbae2cfef4f72069fa73c0b71318c93")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSparser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSparser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
